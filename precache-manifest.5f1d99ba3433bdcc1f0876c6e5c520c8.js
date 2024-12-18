5460819b724563dc8213291a92a93d35898f4cd0
luccarocha <mr.lucca.tec@gmail.com>
2024-12-18 11:15:27 -0300

    Updates

---
+++ precache-manifest.5f1d99ba3433bdcc1f0876c6e5c520c8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d75745674c925c50249f21a1272fc603",
+    "url": "/arvore/index.html"
+  },
+  {
+    "revision": "f70a22f8c3b3b7b1f000",
+    "url": "/arvore/static/css/main.210f3961.chunk.css"
+  },
+  {
+    "revision": "8750177fe397ce33c02f",
+    "url": "/arvore/static/js/2.d46d17e2.chunk.js"
+  },
+  {
+    "revision": "fc0ba07c0f771d66c44444381376eaf8",
+    "url": "/arvore/static/js/2.d46d17e2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "337096b41450407c9d5c",
+    "url": "/arvore/static/js/3.38095475.chunk.js"
+  },
+  {
+    "revision": "f70a22f8c3b3b7b1f000",
+    "url": "/arvore/static/js/main.12cf3a56.chunk.js"
+  },
+  {
+    "revision": "636413e9153c2878af5c",
+    "url": "/arvore/static/js/runtime-main.5bf97ef9.js"
+  }
+]);
