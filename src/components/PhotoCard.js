094ecaa8ebdeea56bc9d781c0a5de3f8fae0fabd
luccarocha <mr.lucca.tec@gmail.com>
2024-12-18 10:53:18 -0300

    Commit Inicial

---
+++ src/components/PhotoCard.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './PhotoCard.css';
+
+const PhotoCard = ({ photo }) => {
+  return (
+    <div className="photo-card">
+      <img src={photo.url} alt={photo.story} />
+      <p>{photo.story}</p>
+    </div>
+  );
+};
